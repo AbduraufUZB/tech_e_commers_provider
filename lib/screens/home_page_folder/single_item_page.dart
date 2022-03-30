d53a3350fe52a72a65be0f87640f973b76b9443a
user <user@Abduraufs-MacBook-Pro.local>
2022-03-29 22:28:33 -0700

    hello

---
+++ lib/screens/home_page_folder/single_item_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class SingleItemPage extends StatelessWidget {
+  const SingleItemPage({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
